2fe0e6dbf90343350b8acdb31fcbcaebc2801e37
Egor Denisov <deneg99@mail.ru>
2018-12-05 18:01:10 +0300

    Add converter.c file

--- converter.c
+++ converter.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main ()
+int main (int argc, char** argv)
 {
+    if (argv != 2)
+    {
+        printf ("Write command line arguments please\n\n");
+        return 0;
+    }
+
     return 0;
 }
